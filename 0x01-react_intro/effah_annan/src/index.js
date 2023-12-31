903156e713089538f8cd137cb21231263d6164d8
Paa Kojo Annan <pkeannan77@gmail.com>
2023-12-31 17:06:18 +0000

    application

--- 0x01-react_intro/effah_annan/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App'
-
-ReactDOM.render(<App />, document.getElementById('root'))
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
